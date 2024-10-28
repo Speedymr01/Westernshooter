7a36eb200bedb80bf83192b5447880657c89659e
Speedymr01 <speedyfizz09@outlook.com>
2024-10-28 15:18:19 +0000

    Fixed pathes (I think)

--- data/Objects.tsx
+++ data/Objects.tsx
@@ -1,62 +1,62 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="800" tileheight="628" tilecount="95" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="800" tileheight="628" tilecount="96" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/dead_tree_1.png" width="156" height="184"/>
+  <image source="../graphics/objects/dead_tree_1.png" width="156" height="184"/>
  </tile>
  <tile id="1">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cacuts_5.png" width="60" height="64"/>
+  <image source="../graphics/objects/cacuts_5.png" width="60" height="64"/>
  </tile>
  <tile id="2">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_4.png" width="60" height="112"/>
+  <image source="../graphics/objects/cactus_4.png" width="60" height="112"/>
  </tile>
  <tile id="3">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_3.png" width="64" height="52"/>
+  <image source="../graphics/objects/cactus_3.png" width="64" height="52"/>
  </tile>
  <tile id="4">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_2.png" width="56" height="44"/>
+  <image source="../graphics/objects/cactus_2.png" width="56" height="44"/>
  </tile>
  <tile id="5">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_1.png" width="56" height="88"/>
+  <image source="../graphics/objects/cactus_1.png" width="56" height="88"/>
  </tile>
  <tile id="6">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/sheriff.png" width="384" height="576"/>
+  <image source="../graphics/objects/sheriff.png" width="384" height="576"/>
  </tile>
  <tile id="7">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/house_2.png" width="608" height="504"/>
+  <image source="../graphics/objects/house_2.png" width="608" height="504"/>
  </tile>
  <tile id="8">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/house.png" width="384" height="484"/>
+  <image source="../graphics/objects/house.png" width="384" height="484"/>
  </tile>
  <tile id="9">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/hotel.png" width="800" height="628"/>
+  <image source="../graphics/objects/hotel.png" width="800" height="628"/>
  </tile>
  <tile id="10">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/bank.png" width="576" height="600"/>
+  <image source="../graphics/objects/bank.png" width="576" height="600"/>
  </tile>
  <tile id="11">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/tree.png" width="252" height="376"/>
+  <image source="../graphics/objects/tree.png" width="252" height="376"/>
  </tile>
  <tile id="12">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/stump_2.png" width="128" height="188"/>
+  <image source="../graphics/objects/stump_2.png" width="128" height="188"/>
  </tile>
  <tile id="13">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/stump.png" width="128" height="128"/>
+  <image source="../graphics/objects/stump.png" width="128" height="128"/>
  </tile>
  <tile id="14">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/skull.png" width="112" height="84"/>
+  <image source="../graphics/objects/skull.png" width="112" height="84"/>
  </tile>
  <tile id="15">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/rock_3.png" width="64" height="56"/>
+  <image source="../graphics/objects/rock_3.png" width="64" height="56"/>
  </tile>
  <tile id="16">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/rock_2.png" width="104" height="64"/>
+  <image source="../graphics/objects/rock_2.png" width="104" height="64"/>
  </tile>
  <tile id="17">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/rock_1.png" width="128" height="108"/>
+  <image source="../graphics/objects/rock_1.png" width="128" height="108"/>
  </tile>
  <tile id="18">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/dead_tree_2.png" width="160" height="156"/>
+  <image source="../graphics/objects/dead_tree_2.png" width="160" height="156"/>
  </tile>
  <tile id="19">
   <image source="../../../Downloads/Free_Desert/Sliced/PNG0008.PNG" width="32" height="32"/>
@@ -285,5 +285,8 @@
  </tile>
  <tile id="94">
   <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_57.png" width="16" height="16"/>
+ </tile>
+ <tile id="95">
+  <image source="../graphics/objects/bank.png" width="576" height="600"/>
  </tile>
 </tileset>
