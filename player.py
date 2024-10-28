d70ed756a336d8aba5c059d0df3a7403a180a271
Speedymr01 <121803606+Speedymr01@users.noreply.github.com>
2024-10-28 10:46:47 +0000

    Up-to-date PATH-fix branch (#6)
    * Buggy end game Logic
    
    * Added end-game logic
    
    * Delete __pycache__ directory
    
    * Fully working Reload (#5)
    
    * Basic reload (not finished)
    
    * Added ammo
    
    * insignificant changes
    
    * Add files via upload
    
    * semi-working reload logic
    
    * still crashing when player reloads
    
    * Fully working Reload (This took sooooo long)
    
    * Delete __pycache__ directory

--- player.py
+++ player.py
@@ -3,14 +3,20 @@ from pygame.math import Vector2 as vector
 from os import walk
 from entity import Entity
 import sys
+import time
 
 class Player(Entity):
     def __init__(self, pos, groups, path, collision_sprites, create_bullet):
         super().__init__(pos, groups, path, collision_sprites)
-        
         self.create_bullet = create_bullet
         self.bullet_shot = False
         self.health = 3
+        self.reloading = False
+        self.reload_start_time = 0
+        self.reload_duration = 4500  # 4.5 seconds
+        self.ammo = 6
+        self.reload_sound = pygame.mixer.Sound('sound/reload.wav')
+        self.score = 0
 
     def get_status(self):
         # idle
@@ -22,9 +28,7 @@ class Player(Entity):
 
     def input(self):
         keys = pygame.key.get_pressed()
-
-        if not self.attacking:    
-
+        if not self.attacking:
             if keys[pygame.K_d]:
                 self.direction.x = 1
                 self.status = 'right'
@@ -33,7 +37,6 @@ class Player(Entity):
                 self.status = 'left'
             else:
                 self.direction.x = 0
-
             if keys[pygame.K_w]:
                 self.direction.y = -1
                 self.status = 'up'
@@ -42,48 +45,61 @@ class Player(Entity):
                 self.status = 'down'
             else:
                 self.direction.y = 0
-
-
+            if keys[pygame.K_r]:
+                self.reload()
             if keys[pygame.K_SPACE]:
-                self.attacking = True
-                self.direction = vector()
-                self.frame_index = 0
-                self.bullet_shot = False
-                match self.status.split('_')[0]:
-                    case 'left': self.bullet_direction = vector(-1,0)
-                    case 'right': self.bullet_direction = vector(1,0)
-                    case 'up': self.bullet_direction = vector(0,-1)
-                    case 'down': self.bullet_direction = vector(0,1)
-                
+                if self.ammo > 0:
+                    self.attacking = True
+                    self.direction = vector()
+                    self.frame_index = 0
+                    self.bullet_shot = False
+                    self.ammo -= 1
+                    match self.status.split('_')[0]:
+                        case 'left': self.bullet_direction = vector(-1,0)
+                        case 'right': self.bullet_direction = vector(1,0)
+                        case 'up': self.bullet_direction = vector(0,-1)
+                        case 'down': self.bullet_direction = vector(0,1)
+
     def animate(self, dt):
         current_animation = self.animations[self.status]
-
         self.frame_index += 7 * dt
-
         if int(self.frame_index) == 2 and self.attacking and not self.bullet_shot:
             bullet_start_pos = self.rect.center + self.bullet_direction * 80
             self.create_bullet(bullet_start_pos, self.bullet_direction)
             self.bullet_shot = True
             self.shoot_sound.play()
-
-        if self.frame_index >=len(current_animation):
+        if self.frame_index >= len(current_animation):
             self.frame_index = 0
             if self.attacking:
                 self.attacking = False
-
         self.image = current_animation[int(self.frame_index)]
         self.mask = pygame.mask.from_surface(self.image)
 
+
+    def reload(self):
+        if self.ammo == 0 and not self.reloading:
+            self.reloading = True
+            self.reload_start_time = pygame.time.get_ticks()
+            self.reload_sound.play()
+
     def check_death(self):
         if self.health <= 0:
+            
             pygame.quit()
             sys.exit()
 
-    def update(self,dt):
+
+    def update(self, dt):
+        if self.reloading:
+            elapsed_time = pygame.time.get_ticks() - self.reload_start_time
+            if elapsed_time >= self.reload_duration:
+                self.reloading = False
+                self.ammo = 6
+        
+        self.get_status()
         self.input()
         self.move(dt)
         self.animate(dt)
         self.blink()
-        self.get_status()
         self.vulnerability_timer()
         self.check_death()
