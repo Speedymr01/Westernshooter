0e70072819f8eafa85a46cb1f22a1172135f8808
duncanhotmail <72929503+duncanhotmail@users.noreply.github.com>
2024-10-28 15:30:08 +0000

    Path fix (#11)
    * Up-to-date PATH-fix branch (#6)
    
    * Buggy end game Logic
    
    * Added end-game logic
    
    * Delete __pycache__ directory
    
    * Fully working Reload (#5)
    
    * Basic reload (not finished)
    
    * Added ammo
    
    * insignificant changes
    
    * Add files via upload
    
    * semi-working reload logic
    
    * still crashing when player reloads
    
    * Fully working Reload (This took sooooo long)
    
    * Delete __pycache__ directory
    
    * Fixed pathes (I think)
    
    * EDITED oBJECTS.TSX TO FIX PATHS
    
    ---------
    
    Co-authored-by: Speedymr01 <121803606+Speedymr01@users.noreply.github.com>
    Co-authored-by: Speedymr01 <speedyfizz09@outlook.com>

--- data/Objects.tsx
+++ data/Objects.tsx
@@ -1,289 +1,61 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="800" tileheight="628" tilecount="95" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="800" tileheight="628" tilecount="19" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/dead_tree_1.png" width="156" height="184"/>
+  <image source="../graphics/objects/dead_tree_1.png" width="156" height="184"/>
  </tile>
  <tile id="1">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cacuts_5.png" width="60" height="64"/>
+  <image source="../graphics/objects/cacuts_5.png" width="60" height="64"/>
  </tile>
  <tile id="2">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_4.png" width="60" height="112"/>
+  <image source="../graphics/objects/cactus_4.png" width="60" height="112"/>
  </tile>
  <tile id="3">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_3.png" width="64" height="52"/>
+  <image source="../graphics/objects/cactus_3.png" width="64" height="52"/>
  </tile>
  <tile id="4">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_2.png" width="56" height="44"/>
+  <image source="../graphics/objects/cactus_2.png" width="56" height="44"/>
  </tile>
  <tile id="5">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/cactus_1.png" width="56" height="88"/>
+  <image source="../graphics/objects/cactus_1.png" width="56" height="88"/>
  </tile>
  <tile id="6">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/sheriff.png" width="384" height="576"/>
+  <image source="../graphics/objects/sheriff.png" width="384" height="576"/>
  </tile>
  <tile id="7">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/house_2.png" width="608" height="504"/>
+  <image source="../graphics/objects/house_2.png" width="608" height="504"/>
  </tile>
  <tile id="8">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/house.png" width="384" height="484"/>
+  <image source="../graphics/objects/house.png" width="384" height="484"/>
  </tile>
  <tile id="9">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/hotel.png" width="800" height="628"/>
+  <image source="../graphics/objects/hotel.png" width="800" height="628"/>
  </tile>
  <tile id="10">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/bank.png" width="576" height="600"/>
+  <image source="../graphics/objects/bank.png" width="576" height="600"/>
  </tile>
  <tile id="11">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/tree.png" width="252" height="376"/>
+  <image source="../graphics/objects/tree.png" width="252" height="376"/>
  </tile>
  <tile id="12">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/stump_2.png" width="128" height="188"/>
+  <image source="../graphics/objects/stump_2.png" width="128" height="188"/>
  </tile>
  <tile id="13">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/stump.png" width="128" height="128"/>
+  <image source="../graphics/objects/stump.png" width="128" height="128"/>
  </tile>
  <tile id="14">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/skull.png" width="112" height="84"/>
+  <image source="../graphics/objects/skull.png" width="112" height="84"/>
  </tile>
  <tile id="15">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/rock_3.png" width="64" height="56"/>
+  <image source="../graphics/objects/rock_3.png" width="64" height="56"/>
  </tile>
  <tile id="16">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/rock_2.png" width="104" height="64"/>
+  <image source="../graphics/objects/rock_2.png" width="104" height="64"/>
  </tile>
  <tile id="17">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/rock_1.png" width="128" height="108"/>
+  <image source="../graphics/objects/rock_1.png" width="128" height="108"/>
  </tile>
  <tile id="18">
-  <image source="../../../Downloads/western+project+files/p15_collisions/graphics/objects/dead_tree_2.png" width="160" height="156"/>
- </tile>
- <tile id="19">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0008.PNG" width="32" height="32"/>
- </tile>
- <tile id="20">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0009.PNG" width="32" height="32"/>
- </tile>
- <tile id="21">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0010.PNG" width="32" height="32"/>
- </tile>
- <tile id="22">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0011.PNG" width="32" height="32"/>
- </tile>
- <tile id="23">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0004.PNG" width="32" height="32"/>
- </tile>
- <tile id="24">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0005.PNG" width="32" height="32"/>
- </tile>
- <tile id="25">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0006.PNG" width="32" height="32"/>
- </tile>
- <tile id="26">
-  <image source="../../../Downloads/Free_Desert/Sliced/PNG0007.PNG" width="32" height="32"/>
- </tile>
- <tile id="27">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_1.png" width="32" height="32"/>
- </tile>
- <tile id="28">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_2.png" width="32" height="32"/>
- </tile>
- <tile id="29">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_3.png" width="32" height="32"/>
- </tile>
- <tile id="30">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_4.png" width="32" height="32"/>
- </tile>
- <tile id="31">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_5.png" width="32" height="32"/>
- </tile>
- <tile id="32">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_6.png" width="32" height="32"/>
- </tile>
- <tile id="33">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_7.png" width="32" height="32"/>
- </tile>
- <tile id="34">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation32x32/végétation32x32_8.png" width="32" height="32"/>
- </tile>
- <tile id="35">
-  <image source="../../../Downloads/WesternAllPack/Signalisation/Signe__15.png" width="32" height="32"/>
- </tile>
- <tile id="36">
-  <image source="../../../Downloads/WesternAllPack/Signalisation/Signe__5.png" width="32" height="32"/>
- </tile>
- <tile id="37">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_58.png" width="16" height="16"/>
- </tile>
- <tile id="38">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_1.png" width="16" height="16"/>
- </tile>
- <tile id="39">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_2.png" width="16" height="16"/>
- </tile>
- <tile id="40">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_3.png" width="16" height="16"/>
- </tile>
- <tile id="41">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_4.png" width="16" height="16"/>
- </tile>
- <tile id="42">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_5.png" width="16" height="16"/>
- </tile>
- <tile id="43">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_6.png" width="16" height="16"/>
- </tile>
- <tile id="44">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_7.png" width="16" height="16"/>
- </tile>
- <tile id="45">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_8.png" width="16" height="16"/>
- </tile>
- <tile id="46">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_9.png" width="16" height="16"/>
- </tile>
- <tile id="47">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_10.png" width="16" height="16"/>
- </tile>
- <tile id="48">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_11.png" width="16" height="16"/>
- </tile>
- <tile id="49">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_12.png" width="16" height="16"/>
- </tile>
- <tile id="50">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_13.png" width="16" height="16"/>
- </tile>
- <tile id="51">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_14.png" width="16" height="16"/>
- </tile>
- <tile id="52">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_15.png" width="16" height="16"/>
- </tile>
- <tile id="53">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_16.png" width="16" height="16"/>
- </tile>
- <tile id="54">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_17.png" width="16" height="16"/>
- </tile>
- <tile id="55">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_18.png" width="16" height="16"/>
- </tile>
- <tile id="56">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_19.png" width="16" height="16"/>
- </tile>
- <tile id="57">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_20.png" width="16" height="16"/>
- </tile>
- <tile id="58">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_21.png" width="16" height="16"/>
- </tile>
- <tile id="59">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_22.png" width="16" height="16"/>
- </tile>
- <tile id="60">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_23.png" width="16" height="16"/>
- </tile>
- <tile id="61">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_24.png" width="16" height="16"/>
- </tile>
- <tile id="62">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_25.png" width="16" height="16"/>
- </tile>
- <tile id="63">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_26.png" width="16" height="16"/>
- </tile>
- <tile id="64">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_27.png" width="16" height="16"/>
- </tile>
- <tile id="65">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_28.png" width="16" height="16"/>
- </tile>
- <tile id="66">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_29.png" width="16" height="16"/>
- </tile>
- <tile id="67">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_30.png" width="16" height="16"/>
- </tile>
- <tile id="68">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_31.png" width="16" height="16"/>
- </tile>
- <tile id="69">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_32.png" width="16" height="16"/>
- </tile>
- <tile id="70">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_33.png" width="16" height="16"/>
- </tile>
- <tile id="71">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_34.png" width="16" height="16"/>
- </tile>
- <tile id="72">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_35.png" width="16" height="16"/>
- </tile>
- <tile id="73">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_36.png" width="16" height="16"/>
- </tile>
- <tile id="74">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_37.png" width="16" height="16"/>
- </tile>
- <tile id="75">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_38.png" width="16" height="16"/>
- </tile>
- <tile id="76">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_39.png" width="16" height="16"/>
- </tile>
- <tile id="77">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_40.png" width="16" height="16"/>
- </tile>
- <tile id="78">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_41.png" width="16" height="16"/>
- </tile>
- <tile id="79">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_42.png" width="16" height="16"/>
- </tile>
- <tile id="80">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_43.png" width="16" height="16"/>
- </tile>
- <tile id="81">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_44.png" width="16" height="16"/>
- </tile>
- <tile id="82">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_45.png" width="16" height="16"/>
- </tile>
- <tile id="83">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_46.png" width="16" height="16"/>
- </tile>
- <tile id="84">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_47.png" width="16" height="16"/>
- </tile>
- <tile id="85">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_48.png" width="16" height="16"/>
- </tile>
- <tile id="86">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_49.png" width="16" height="16"/>
- </tile>
- <tile id="87">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_50.png" width="16" height="16"/>
- </tile>
- <tile id="88">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_51.png" width="16" height="16"/>
- </tile>
- <tile id="89">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_52.png" width="16" height="16"/>
- </tile>
- <tile id="90">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_53.png" width="16" height="16"/>
- </tile>
- <tile id="91">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_54.png" width="16" height="16"/>
- </tile>
- <tile id="92">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_55.png" width="16" height="16"/>
- </tile>
- <tile id="93">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_56.png" width="16" height="16"/>
- </tile>
- <tile id="94">
-  <image source="../../../Downloads/WesternAllPack/Végetation/Vegetation 16x16/Vegetation 16x16_57.png" width="16" height="16"/>
+  <image source="../graphics/objects/dead_tree_2.png" width="160" height="156"/>
  </tile>
 </tileset>
